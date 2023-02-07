5a96eb5124f9a4e60070d46a12b91bdfb784dbe0
Gabriel Alao <gabrieldeveloper1@gmail.com>
2023-02-07 12:03:21 -0600

    update button label

--- lib/views/post_screen.dart
+++ lib/views/post_screen.dart
@@ -20,56 +20,54 @@ class _PostScreenState extends State<PostScreen> {
       return Container(
           padding: const EdgeInsets.all(12),
           color: Colors.white.withOpacity(0.9),
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          controller: titleCtlr,
-                          decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.all(16),
-                              border: const OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10.0),
-                                ),
-                              ),
-                              filled: true,
-                              hintStyle: const TextStyle(color: Colors.grey),
-                              hintText: "Please input post title...",
-                              fillColor: Colors.grey[200]),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue, // background
-                          foregroundColor: Colors.white, // foreground
-                        ),
-                        onPressed: () {
-                          if (titleCtlr.text == '') {
-                            Fluttertoast.showToast(
-                                msg: "Empty post title, please input title.",
-                                toastLength: Toast.LENGTH_SHORT,
-                                gravity: ToastGravity.CENTER,
-                                timeInSecForIosWeb: 1,
-                                backgroundColor: Colors.red,
-                                textColor: Colors.white,
-                                fontSize: 16.0);
-                            return;
-                          }
-                          Provider.of<PostProvider>(context, listen: false)
-                              .addNewPost(titleCtlr.text);
-                          titleCtlr.clear();
-                        },
-                        child: const Text('Add'),
-                      )
-                    ]),
-                Expanded(
-                  child: postProvider.isEmpty()
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: titleCtlr,
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(16),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                          filled: true,
+                          hintStyle: const TextStyle(color: Colors.grey),
+                          hintText: "Please input post title...",
+                          fillColor: Colors.grey[200]),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // background
+                      foregroundColor: Colors.white, // foreground
+                    ),
+                    onPressed: () {
+                      if (titleCtlr.text == '') {
+                        Fluttertoast.showToast(
+                            msg: "Empty post title, please input title.",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                        return;
+                      }
+                      Provider.of<PostProvider>(context, listen: false)
+                          .addNewPost(titleCtlr.text);
+                      titleCtlr.clear();
+                    },
+                    child: const Text('Post'),
+                  )
+                ]),
+            Expanded(
+                child: postProvider.isEmpty()
                     ? const Center(
                         child: Text("Empty posts, Please add new post..."))
                     : ListView.builder(
@@ -78,9 +76,8 @@ class _PostScreenState extends State<PostScreen> {
                         itemBuilder: (BuildContext context, int index) {
                           return PostListTile(post: postProvider.posts[index]);
                         },
-                      )
-                    )
-              ]));
+                      ))
+          ]));
     });
   }
 }
